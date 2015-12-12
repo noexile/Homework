689f3b4259a3ba7159f4c877b40b674988470929
Alexander Zorov - nc office <alexander.zorov@gmail.com>
2015-12-12 10:35:10 +0200

    Working demo - testing incapsulation

---
+++ Lesson 16 - Constructiors, this keyword, Encapsulation, Access modifiers, getters, setters, static, final/WorkDemo/Task.java
@@ -0,0 +1,36 @@
+package WorkDemo;
+
+public class Task {
+	
+	private String taskName;
+	private int workingHours;
+	
+	Task(String taskName, int workingHours) {
+		setTaskName(taskName);
+		setWorkingHours(workingHours);
+	}
+	
+	public String getTaskName() {
+		return taskName;
+	}
+	
+	public void setTaskName(String taskName) {
+		if (taskName.trim().isEmpty()) {
+			System.out.println("You cannot input empty task name!");
+			return;
+		}
+		this.taskName = taskName;
+	}
+	
+	public int getWorkingHours() {
+		return workingHours;
+	}
+	
+	public void setWorkingHours(int workingHours) {
+		if (workingHours < 1) {
+			return;
+		}
+		this.workingHours = workingHours;
+	}
+	
+}
---
+++ Lesson 16 - Constructiors, this keyword, Encapsulation, Access modifiers, getters, setters, static, final/WorkDemo/WorkDemo.java
@@ -0,0 +1,14 @@
+package WorkDemo;
+
+public class WorkDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO functionality of class Task and Employee
+		Task task = new Task("lepi plochki", 11);
+
+		Employee ivan = new Employee("bai Ivan");
+		
+	}
+
+}
